fd40dd089653b81ccba2a9bc416c07d5b045d902
danny gersh <dannygershman96@gmail.com>
2024-08-18 07:58:32 +0300

    added mui

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { get, post } from 'src/fetch';
+import Button from '@mui/material/Button';
 import './index.css';
 
 const App: React.FC = () => {
@@ -10,7 +11,12 @@ const App: React.FC = () => {
     post('/test', { test: 'test' })
       .then(res => console.log(res))
   }, [])
-  return <div className='p-8'>Hello, world!</div>;
+  return <>
+    <div className='p-8'>Hello, world!</div>
+    <Button variant="contained" color="primary">
+      MUI Button
+    </Button>
+  </>
 };
 
 //@ts-ignore
