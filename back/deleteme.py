ce987697547115d7f08156b1857fb1611ff38734
dannyGersh <dannygersh96@gmail.com>
2024-10-13 13:57:07 +0100

    dont deleteme

--- back/deleteme.py
+++ back/deleteme.py
@@ -1,10 +1,18 @@
 from PIL import Image
 from rpi_ws281x import PixelStrip, Color
 import time
+import re
 
-LED_COUNT = 32*7
+LED_COUNT = 3
 LED_PIN = 18   
 TIME_PER_GIF_IN_SEC = 10
+MIN_ALLOWED_DELAY_PER_GIF = 0.01
+
+def clear_strip(strip):
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, Color(0, 0, 0))  
+    strip.show()
+
 
 def rgba_to_rgb(pixel):
     alpha = pixel[3] / 255.0
@@ -17,10 +25,10 @@ def readGif(fileName):
     gif = Image.open(fileName)  
     frame_number = gif.n_frames  
     frames = []
-
-    for i in range(1, frame_number):
+    
+    for i in range(0, frame_number):
         gif.seek(i)  
-        frame_data = list(gif.getdata())
+        frame_data = list(gif.convert("RGBA").getdata())
         frame = []
         for y in range(gif.height):
             col = []
@@ -37,6 +45,15 @@ def playGif(fileName):
     strip = PixelStrip(LED_COUNT, LED_PIN)
     start_time = time.time()
     
+    delay = 0.1
+    try:
+        delay = float(re.search(r'(?<=__)\d(\.\d+?)?(?=__)', fileName).group())
+        if delay < MIN_ALLOWED_DELAY_PER_GIF:
+            delay = MIN_ALLOWED_DELAY_PER_GIF
+    except Exception as e:
+        print(e)
+        pass
+    
     while time.time() - start_time < TIME_PER_GIF_IN_SEC:
         for f in frames:
             i = 0
@@ -46,7 +63,17 @@ def playGif(fileName):
                     strip.setPixelColor(i, Color(x[0], x[1], x[2]))
                     i += 1
             strip.show()
-            time.sleep(0.1)
+            time.sleep(delay)
+    
+    clear_strip(strip)
     
 if __name__ == '__main__':
-    playGif('/mnt/gifs/gifs/NewPiskel.gif')
+    try:
+        strip = PixelStrip(LED_COUNT, LED_PIN)
+        #strip.begin()  # Initialize the strip
+        #strip.setPixelColor(0, Color(0, 255, 0))
+        #strip.show()
+        print("LED strip initialized successfully.")
+        #playGif('/mnt/gifs/gifs/NewPiskel__0.01__.gif')
+    except Exception as e:
+        print("AAAAAAAAAAAAAAAA", e)
--- back/readGif.py
+++ back/readGif.py
@@ -1,5 +1,4 @@
 from PIL import Image
-import numpy as np
 
 def readGif(fileName):
     gif = Image.open(fileName)
@@ -7,23 +6,23 @@ def readGif(fileName):
     frame_number = 0
     frames = []
 
-    while True:
-        try:
-            gif.seek(frame_number)  # Go to the frame
-            frame_array = np.array(gif)  # Convert the frame to a NumPy array
-            frames.append(frame_array)  # Store the frame array
-            frame_number += 1  # Increment the frame number
-            # print(f'Frame {frame_number} pixels:')
-            # print(frame_array)
-        except EOFError:
-            break  # Exit the loop when all frames are processed
-        except OSError:
-            print(f"Error loading frame {frame_number}, skipping...")
-            frame_number += 1  # Skip the problematic frame and continue
+    # while True:
+    #     try:
+    #         gif.seek(frame_number)  # Go to the frame
+    #         frame_array = np.array(gif)  # Convert the frame to a NumPy array
+    #         frames.append(frame_array)  # Store the frame array
+    #         frame_number += 1  # Increment the frame number
+    #         # print(f'Frame {frame_number} pixels:')
+    #         # print(frame_array)
+    #     except EOFError:
+    #         break  # Exit the loop when all frames are processed
+    #     except OSError:
+    #         print(f"Error loading frame {frame_number}, skipping...")
+    #         frame_number += 1  # Skip the problematic frame and continue
 
-    frames = frames[1:len(frames)]
+    # frames = frames[1:len(frames)]
 
-    return frames
+    return []
 
 
 
